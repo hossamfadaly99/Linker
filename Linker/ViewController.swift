7380a9fef1776cb3dc7f38e044ec89ef872ec74b
Hossam Fadaly <74186546+hossamfadaly99@users.noreply.github.com>
2023-10-07 11:37:30 +0300

    initial setup for authentication collectionView

--- Linker/ViewController.swift
+++ Linker/ViewController.swift
@@ -13,21 +13,46 @@ class ViewController: UIViewController {
   @IBOutlet weak var usernameTF: AkiraTextField!
   @IBOutlet weak var passwordTF: AkiraTextField!
   @IBOutlet weak var mailTF: AkiraTextField!
+  @IBOutlet weak var collectionView: UICollectionView!
 
   private var isPasswordVisable = false
 
-  @IBOutlet weak var userTF: UITextField!
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    setupCollecionView()
+
 //    passwordTF.borderColor = .red
 //    passwordTF.placeholderColor = .red
+  }
+
+  private func setupCollecionView() {
+    collectionView.dataSource = self
+    collectionView.delegate = self
   }
 
   @IBAction func changePasswordVisability(_ sender: UIButton) {
     isPasswordVisable.toggle()
     passwordTF.isSecureTextEntry = !isPasswordVisable
   }
-
 }
 
+extension ViewController: UICollectionViewDelegate {}
+
+extension ViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    2
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "formCell", for: indexPath)
+
+    return cell
+  }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: collectionView.frame.size.width, height: 350)
+  }
+}
